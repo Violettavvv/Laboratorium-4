cca0584c3d9f150d9cf95573f3a73d54efde7f99
Violetta Volodina <dsw48795@student.dsw.edu.pl>
2023-05-03 00:56:33 +0200

    Dodano nowe flagi

--- help.sh
+++ help.sh
@@ -1,23 +1,9 @@
 !/bin/bash
 
-if [[ $1 == "--help" ]]
+if [[ $1 == "--help" || $2 == "-h" ]]
 then
   echo "Dostępne opcje:"
-  echo "--logs N   - utworzenie N plików logów, gdzie N to liczba całkowita"
+  echo "--logs N, -l  - utworzenie N plików logów, gdzie N to liczba całkowita"
+  echo "--date, -d - wyświetlenie dzisiejszej daty"
   exit 0
 fi
-
-num_of_logs=$2
-
-if [ -z "$num_of_logs" ]
-then
-  echo "Nie podano liczby plików do utworzenia"
-  exit 1
-fi
-
-for i in $(seq 1 $num_of_logs)
-do
-  echo "Nazwa pliku: log$i.txt" >> log$i.txt
-  echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
-  echo "Data: $(date)" >> log$i.txt
-done
