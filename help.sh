2b22b67a0c4b43a4f510f8c4eafa55b878bdea38
Violettavvv <131534648+Violettavvv@users.noreply.github.com>
2023-05-05 02:25:07 +0200

    Dodano nowe opcje.

--- help.sh
+++ help.sh
@@ -3,7 +3,9 @@
 if [[ $1 == "--help" || $2 == "-h" ]]
 then
   echo "Dostępne opcje:"
-  echo "--logs N, -l  - utworzenie N plików logów, gdzie N to liczba całkowita"
   echo "--date, -d - wyświetlenie dzisiejszej daty"
+  echo "--logs N, -l  - utworzenie N plików logów, gdzie N to liczba całkowita"
+  echo "--init   - klonuje całe repozytorium do katalogu"
+  echo "-e, --error   - tworzy error pliki"
   exit 0
 fi
